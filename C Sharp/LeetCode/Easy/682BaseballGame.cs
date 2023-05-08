052a8c1d53ce67abd086f5198ed505efde789a7e
proninp <proninpaul@yandex.ru>
2023-05-08 15:59:49 +0300

    LeetCode C# 682. Baseball Game

---
+++ C Sharp/LeetCode/Easy/682BaseballGame.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    /*
+     * 682. Baseball Game
+     * You are keeping the scores for a baseball game with strange rules.
+     * At the beginning of the game, you start with an empty record.
+     * You are given a list of strings operations,
+     * where operations[i] is the ith operation you must apply to the record and is one of the following:
+     * An integer x - Record a new score of x.
+     * '+' - Record a new score that is the sum of the previous two scores.
+     * 'D' - Record a new score that is the double of the previous score.
+     * 'C' - Invalidate the previous score, removing it from the record.
+     * Return the sum of all the scores on the record after applying all the operations.
+     * The test cases are generated such that the answer and all intermediate calculations fit in a 32-bit integer and that all operations are valid.
+     */
+    internal class _682BaseballGame
+    {
+        public static int CalPoints(string[] operations)
+        {
+            int len = operations.Length;
+            int j = 0;
+            int[] arr = new int[len];
+            for (int i = 0; i < len; i++)
+            {
+                if (operations[i] == "C")
+                {
+                    arr[--j] = 0;
+                }
+                else if (operations[i] == "D")
+                {
+                    arr[j] = arr[j - 1] * 2;
+                    j++;
+                }
+                else if (operations[i] == "+")
+                {
+                    arr[j] = arr[j - 1] + arr[j - 2];
+                    j++;
+                }
+                else
+                    arr[j++] = Convert.ToInt32(operations[i]);
+            }
+            return arr.Sum();
+        }
+    }
+}
--- C Sharp/LeetCode/Program.cs
+++ C Sharp/LeetCode/Program.cs
@@ -12,35 +12,7 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            TreeNode root = new TreeNode(3);
-            root.left = new TreeNode(9);
-            root.right = new TreeNode(20);
-            root.right.left = new TreeNode(15);
-            root.right.right = new TreeNode(7);
-
-            TreeNode root2 = new TreeNode(2);
-            root2.right = new TreeNode(3);
-            root2.right.right = new TreeNode(4);
-            root2.right.right.right = new TreeNode(5);
-            root2.right.right.right.right = new TreeNode(6);
-
-            TreeNode root3 = new TreeNode(1);
-            root3.left = new TreeNode(2);
-            root3.right = new TreeNode(3);
-            root3.left.left = new TreeNode(4);
-            root3.right.left = new TreeNode(5);
-
-            TreeNode root4 = new TreeNode(5);
-            root4.left = new TreeNode(4);
-            root4.right = new TreeNode(8);
-            root4.left.left = new TreeNode(11);
-            root4.left.left.left = new TreeNode(7);
-            root4.left.left.right = new TreeNode(2);
-            root4.right.left = new TreeNode(13);
-            root4.right.right = new TreeNode(7);
-            root4.right.right.right = new TreeNode(1);
-
-            Console.WriteLine(TreeNodePathSum.HasPathSum(root4, 27));
+            Console.WriteLine(_682BaseballGame.CalPoints(new string[] { "5", "2", "C", "D", "+" }));
             Console.ReadLine();
         }
         static string PrintArray(int[] a)
