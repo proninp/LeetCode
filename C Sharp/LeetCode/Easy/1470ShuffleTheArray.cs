d481f46424fdd0680c646c9503cdb05c8f5e7b70
proninp <proninpaul@yandex.ru>
2023-05-11 08:33:17 +0300

    LeetCode C# 1470. Shuffle the Array

---
+++ C Sharp/LeetCode/Easy/1470ShuffleTheArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    /*
+     * 1470. Shuffle the Array
+     * Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+     * Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+     */
+    internal class _1470ShuffleTheArray
+    {
+        public static int[] Shuffle(int[] nums, int n)
+        {
+            int[] ans = new int[nums.Length];
+            int j = 0;
+            for (int i = 0; i < n; i++)
+            {
+                ans[j++] = nums[i];
+                ans[j++] = nums[i + n];
+            }
+            return ans;
+        }
+    }
+}
