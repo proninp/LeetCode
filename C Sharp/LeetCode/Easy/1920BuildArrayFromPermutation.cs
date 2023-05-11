5780aae0f860a69e1c95c8c8e958120e45e9b9d8
proninp <proninpaul@yandex.ru>
2023-05-11 08:23:32 +0300

    LeetCode C# 1920. Build Array from Permutation

---
+++ C Sharp/LeetCode/Easy/1920BuildArrayFromPermutation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    /*
+     * 1920. Build Array from Permutation
+     * Given a zero-based permutation nums (0-indexed),
+     * build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+     * A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+     */
+    internal class _1920BuildArrayFromPermutation
+    {
+        public static int[] BuildArray(int[] nums)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+                list.Add(nums[nums[i]]);
+            return list.ToArray();
+        }
+    }
+}
