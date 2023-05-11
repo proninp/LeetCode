eb548e1580c8cc641157b4ef448ecf5d18ea2d9a
proninp <proninpaul@yandex.ru>
2023-05-11 08:42:06 +0300

    LeetCode C# 2011. Final Value of Variable After Performing Operations

---
+++ C Sharp/LeetCode/Easy/2011FinalValueOfVariableAfterPerformingOperations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    /*
+     * 2011. Final Value of Variable After Performing Operations
+     * There is a programming language with only four operations and one variable X:
+     * ++X and X++ increments the value of the variable X by 1.
+     * --X and X-- decrements the value of the variable X by 1.
+     * Initially, the value of X is 0.
+     * Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations
+     */
+    internal class _2011FinalValueOfVariableAfterPerformingOperations
+    {
+        public static int FinalValueAfterOperations(string[] operations)
+        {
+            int res = 0;
+            foreach (string operation in operations)
+                res += (operation[1] == '+') ? 1 : -1; 
+            return res;
+        }
+    }
+}
