1f520cebe0fa6b303366fb8300e809cc220d2599
proninp <proninpaul@yandex.ru>
2023-05-16 09:18:58 +0300

    C# 392. Is Subsequence

---
+++ C Sharp/LeetCode/Easy/392IsSubsequence.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    /*
+     * 392. Is Subsequence
+     * Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+     * A subsequence of a string is a new string that is formed from the original string by deleting some (can be none)
+     * of the characters without disturbing the relative positions of the remaining characters.
+     * (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+     */
+    internal class _392IsSubsequence
+    {
+        public static bool IsSubsequence(string s, string t)
+        {
+            int sInd = 0;
+            int tInd = 0;
+            while(sInd < s.Length && tInd < t.Length)
+            {
+                if (t[tInd] == s[sInd])
+                    sInd++;
+                tInd++;
+            }
+            return sInd == s.Length;
+        }
+    }
+}
