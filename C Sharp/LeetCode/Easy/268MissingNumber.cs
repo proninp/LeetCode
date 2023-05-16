acd7689b3aa61ed3b3eaf4ec695cb09e7e822293
proninp <proninpaul@yandex.ru>
2023-05-16 19:21:41 +0300

    C# 268. Missing Number

---
+++ C Sharp/LeetCode/Easy/268MissingNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    /*
+     * 268. Missing Number
+     * Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+     */
+    internal class _268MissingNumber
+    {
+        public static int MissingNumber(int[] nums)
+        {
+            int len = nums.Length;
+            return len * (len + 1) / 2 - nums.Sum();
+        }
+    }
+}
