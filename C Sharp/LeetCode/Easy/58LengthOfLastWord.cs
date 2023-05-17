7a103ee42e019609a96a82c8ffa569b5546a4d93
proninp <proninpaul@yandex.ru>
2023-05-17 09:49:52 +0300

    C# 58. Length of Last Word

---
+++ C Sharp/LeetCode/Easy/58LengthOfLastWord.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    /*
+     * 58. Length of Last Word
+     * Given a string s consisting of words and spaces, return the length of the last word in the string.
+     * A word is a maximal substring consisting of non-space characters only.
+     */
+    internal class _58LengthOfLastWord
+    {
+        public static int LengthOfLastWord(string s)
+        {
+            int len = 0;
+            for (int i = s.Length - 1; i >= 0 ; i--)
+            {
+                if (s[i] == ' ')
+                {
+                    if (len != 0)
+                        return len;
+                }
+                else
+                    len++;
+            }
+            return len;
+        }
+    }
+}
