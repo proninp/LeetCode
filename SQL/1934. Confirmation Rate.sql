e6feacf7240e3636458ed59e5b4a7cf328e60c92
proninp <proninpaul@yandex.ru>
2023-05-22 09:05:26 +0300

    SQL 1934. Confirmation Rate

---
+++ SQL/1934. Confirmation Rate.sql
@@ -0,0 +1,36 @@
+/*
+ * 1934. Confirmation Rate
+ * Table: Signups
+ * +----------------+----------+
+ * | Column Name    | Type     |
+ * +----------------+----------+
+ * | user_id        | int      |
+  * * | time_stamp     | datetime |
+ * +----------------+----------+
+ * user_id is the primary key for this table.
+ * Each row contains information about the signup time for the user with ID user_id.
+
+ * Table: Confirmations
+ * +----------------+----------+
+ * | Column Name    | Type     |
+ * +----------------+----------+
+ * | user_id        | int      |
+ * | time_stamp     | datetime |
+ * | action         | ENUM     |
+ * +----------------+----------+
+ * (user_id, time_stamp) is the primary key for this table.
+ * user_id is a foreign key with a reference to the Signups table.
+ * action is an ENUM of the type ('confirmed', 'timeout')
+ * Each row of this table indicates that the user with ID user_id requested a confirmation message at time_stamp and that confirmation message was either confirmed ('confirmed') or expired without confirming ('timeout').
+ 
+ * The confirmation rate of a user is the number of 'confirmed' messages divided by
+ * the total number of requested confirmation messages.
+ * The confirmation rate of a user that did not request any confirmation messages is 0. Round the confirmation rate to two decimal places.
+ * Write an SQL query to find the confirmation rate of each user.
+ * Return the result table in any order.
+ */
+SELECT s.user_id
+        , ROUND((SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END) * 1.0) / COUNT(s.user_id), 2) confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c ON s.user_id = c.user_id
+GROUP BY s.user_id
