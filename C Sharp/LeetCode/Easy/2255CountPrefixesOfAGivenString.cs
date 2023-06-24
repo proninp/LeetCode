39516ef02d8081797e7e577ef3a800c11c6232de
proninp <proninpaul@yandex.ru>
2023-06-24 11:30:54 +0300

    C# 2255. Count Prefixes of a Given String

---
+++ C Sharp/LeetCode/Easy/2255CountPrefixesOfAGivenString.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    /*
+     * 2255. Count Prefixes of a Given String
+     * You are given a string array words and a string s, where words[i] and s comprise only of lowercase English letters.
+     * Return the number of strings in words that are a prefix of s.
+     * A prefix of a string is a substring that occurs at the beginning of the string. A substring is a contiguous sequence of characters within a string.
+     */
+    internal class _2255CountPrefixesOfAGivenString
+    {
+        public int CountPrefixes(string[] words, string s)
+        {
+            int cnt = 0;
+            int sLen = s.Length;
+            foreach(var word in words)
+            {
+                if (sLen >= word.Length)
+                    if (s.StartsWith(word))
+                        cnt++;
+            }
+            return cnt;
+        }
+    }
+}
