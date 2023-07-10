93cde8f642df563f0138b611dc7e3ddc1788a0ca
proninp <proninpaul@yandex.ru>
2023-07-10 23:31:24 +0300

    C# 3. Longest Substring Without Repeating Characters

---
+++ C Sharp/LeetCode/Medium/3LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Medium
+{
+    /*
+     * 3. Longest Substring Without Repeating Characters
+     * Given a string s, find the length of the longest substring without repeating characters.
+     */
+    internal class _3LongestSubstringWithoutRepeatingCharacters
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            var set = new HashSet<char>();
+            var maxSubst = 0;
+            int l = 0;
+            for (int r = 0; r < s.Length; r++)
+            {
+                while(set.Contains(s[r]))
+                {
+                    set.Remove(s[l]);
+                    l++;
+                }
+                set.Add(s[r]);
+                maxSubst = Math.Max(maxSubst, r - l + 1);
+            }
+            return maxSubst;
+        }
+    }
+}
