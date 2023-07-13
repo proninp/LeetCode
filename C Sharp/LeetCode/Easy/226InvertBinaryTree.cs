fe3ef09b293bb72fe034f201b7f628de3006530d
proninp <proninpaul@yandex.ru>
2023-07-13 08:34:03 +0300

    C# 226. Invert Binary Tree

---
+++ C Sharp/LeetCode/Easy/226InvertBinaryTree.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    /*
+     * 226. Invert Binary Tree
+     * Given the root of a binary tree, invert the tree, and return its root.
+     */
+    internal class _226InvertBinaryTree
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+                return root;
+            (root.left, root.right) = (root.right, root.left);
+            InvertTree(root.left);
+            InvertTree(root.right);
+            return root;
+        }
+    }
+}
