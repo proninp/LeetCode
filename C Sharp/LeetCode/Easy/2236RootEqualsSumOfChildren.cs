0c9544717bbf6e3e171b87b0d8ea08ab2e916b2d
proninp <proninpaul@yandex.ru>
2023-07-16 09:58:18 +0300

    C# 2236. Root Equals Sum of Children

---
+++ C Sharp/LeetCode/Easy/2236RootEqualsSumOfChildren.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    /*
+     * 2236. Root Equals Sum of Children
+     * You are given the root of a binary tree that consists of exactly 3 nodes: the root, its left child, and its right child.
+     * Return true if the value of the root is equal to the sum of the values of its two children, or false otherwise.
+     */
+    internal class _2236RootEqualsSumOfChildren
+    {
+        public bool CheckTree(TreeNode root) => root.val == (root.left.val + root.right.val);
+    }
+}
