328b96a45a89963db83bbef937859342e40a24b6
proninp <proninpaul@yandex.ru>
2023-07-19 09:02:08 +0300

    C# 617. Merge Two Binary Trees

---
+++ C Sharp/LeetCode/Easy/617MergeTwoBinaryTrees.cs
@@ -0,0 +1,36 @@
+﻿using LeetCode.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    /*
+     * 617. Merge Two Binary Trees
+     * You are given two binary trees root1 and root2.
+     * Imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not.
+     * You need to merge the two trees into a new binary tree.
+     * The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node.
+     * Otherwise, the NOT null node will be used as the node of the new tree.
+     * Return the merged tree.
+     * Note: The merging process must start from the root nodes of both trees.
+     */
+    internal class _617MergeTwoBinaryTrees
+    {
+        public TreeNode MergeTrees(TreeNode root1, TreeNode root2)
+        {
+            if (root1 == null && root2 == null)
+                return null;
+            if (root1 == null)
+                return root2;
+            if (root2 == null)
+                return root1;
+            root1.val += root2.val;
+            root1.left = MergeTrees(root1.left, root2.left);
+            root1.right = MergeTrees(root1.right, root2.right);
+            return root1;
+        }
+    }
+}
