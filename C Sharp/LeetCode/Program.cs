b562499524f9f239b60e7fb4bbd639c165a8fca1
proninp <proninpaul@yandex.ru>
2023-07-20 09:15:10 +0300

    C# 5. Longest Palindromic Substring

--- C Sharp/LeetCode/Program.cs
+++ C Sharp/LeetCode/Program.cs
@@ -1,10 +1,5 @@
-﻿using LeetCode.Easy;
-using LeetCode.Medium;
+﻿using LeetCode.Medium;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using LeetCode.Common;
 
 namespace LeetCode
 {
@@ -12,24 +7,12 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
+            var s = "babad";
 
-            var sudokuBoard = new char[][]
-            {
-                new char[] { '5','3','.','.','7','.','.','.','.' },
-                new char[] { '6','.','.','1','9','5','.','.','.' },
-                new char[] { '.','9','8','.','.','.','.','6','.' },
-                new char[] { '8','.','.','.','6','.','.','.','3' },
-                new char[] { '4','.','.','8','.','3','.','.','1' },
-                new char[] { '7','.','.','.','2','.','.','.','6' },
-                new char[] { '.','6','.','.','.','.','2','8','.' },
-                new char[] { '.','.','.','4','1','9','.','.','5' },
-                new char[] { '.','.','.','.','8','.','.','7','9' }
-            };
-
-            var a = _36ValidSudoku.IsValidSudoku(sudokuBoard);
+            var a = _5LongestPalindromicSubstring.LongestPalindrome(s);
             Console.WriteLine(a);
             Console.ReadLine();
         }
-        
+
     }
 }
