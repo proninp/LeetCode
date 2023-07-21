8ef1019b840f332fec763a0e0bf6a457def7307c
proninp <proninpaul@yandex.ru>
2023-07-21 16:55:54 +0300

    C# 2273. Find Resultant Array After Removing Anagrams

---
+++ C Sharp/LeetCode/Easy/2273FindResultantArrayAfterRemovingAnagrams.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    /*
+     * 2273. Find Resultant Array After Removing Anagrams
+     * You are given a 0-indexed string array words, where words[i] consists of lowercase English letters.
+     * In one operation, select any index i such that 0 < i < words.length and words[i - 1] and words[i] are anagrams,
+     * and delete words[i] from words. Keep performing this operation as long as you can select an index that satisfies the conditions.
+     * Return words after performing all operations.
+     * It can be shown that selecting the indices for each operation in any arbitrary order will lead to the same result.
+     * An Anagram is a word or phrase formed by rearranging the letters of
+     * a different word or phrase using all the original letters exactly once.
+     * For example, "dacb" is an anagram of "abdc".
+     */
+    internal class _2273FindResultantArrayAfterRemovingAnagrams
+    {
+        public IList<string> RemoveAnagrams(string[] words)
+        {
+            var list = new List<string>() { words[0] };
+            var m1 = GetMap(words[0]);
+            for (int i = 1; i < words.Length; i++)
+            {
+                var m2 = GetMap(words[i]);
+                if (!IsMapEquals(m1, m2))
+                {
+                    list.Add(words[i]);
+                    m1 = m2;
+                }
+            }
+            return list;
+        }
+        private int[] GetMap(string s)
+        {
+            int[] map = new int[26];
+            foreach (char c in s)
+                map[c - 'a']++;
+            return map;
+        }
+        private bool IsMapEquals(int[] arr1, int[] arr2)
+        {
+            for (int i = 0; i < arr1.Length; i++)
+                if (arr1[i] != arr2[i])
+                    return false;
+            return true;
+        }
+    }
+}
---
+++ C Sharp/LeetCode/Medium/75SortColors.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Medium
+{
+    /*
+     * 75. Sort Colors
+     * Given an array nums with n objects colored red, white, or blue,
+     * sort them in-place so that objects of the same color are adjacent,
+     * with the colors in the order red, white, and blue.
+     * We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+     * You must solve this problem without using the library's sort function.
+     */
+    internal class _75SortColors
+    {
+        public void SortColors(int[] nums)
+        {
+            var map = new int[3];
+            foreach (var n in nums)
+                map[n]++;
+            int i = 0;
+            for (int j = 0; j < map.Length; j++)
+                while (map[j]-- > 0)
+                    nums[i++] = j;
+        }
+    }
+}
