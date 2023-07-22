bcf216345a539a720cbf5a145be5c9a1e97a15d0
proninp <proninpaul@yandex.ru>
2023-07-22 10:12:30 +0300

    C# 119. Pascal's Triangle II

---
+++ C Sharp/LeetCode/Easy/119PascalsTriangleII.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    /*
+     * 119. Pascal's Triangle II
+     * Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+     * In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+     */
+    internal class _119PascalsTriangleII
+    {
+        public IList<int> GetRow(int rowIndex)
+        {
+            var list = new List<int>();
+            for (int i = 0; i <= rowIndex; i++)
+            {
+                list.Add(1);
+                for (int j = i - 1; j > 0; j--)
+                    list[j] = list[j] + list[j - 1];
+            }
+            return list;
+        }
+    }
+}
