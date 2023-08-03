22c282022afe63d8c247b80693970d65c8ee6b29
proninp <proninpaul@yandex.ru>
2023-08-03 21:57:19 +0300

    C# 1108. Defanging an IP Address

---
+++ C Sharp/LeetCode/Easy/1108DefangingAnIPAddress.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace LeetCode.Easy
+{
+    /*
+     * 1108. Defanging an IP Address
+     * Given a valid (IPv4) IP address, return a defanged version of that IP address.
+     * A defanged IP address replaces every period "." with "[.]".
+     */
+    public class _1108DefangingAnIPAddress
+    {
+        public string DefangIPaddr(string address)
+        {
+            var sb = new StringBuilder(address.Length);
+            foreach (var c in address)
+            {
+                if (c.Equals('.'))
+                    sb.Append("[.]");
+                else
+                    sb.Append(c);
+            }
+            return sb.ToString();
+        }
+    }
+}
