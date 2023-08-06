342caaf2f9debc3f1db423a952fc997f43d954d4
proninp <proninpaul@yandex.ru>
2023-08-06 09:23:34 +0300

    C# 2769. Find the Maximum Achievable Number

---
+++ C Sharp/LeetCode/Easy/2769FindTheMaximumAchievableNumber.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Easy
+{
+    /*
+     * 2769. Find the Maximum Achievable Number
+     * You are given two integers, num and t.
+     * An integer x is called achievable if it can become equal to num after applying the following operation no more than t times:
+     * Increase or decrease x by 1, and simultaneously increase or decrease num by 1.
+     * Return the maximum possible achievable number. It can be proven that there exists at least one achievable number.
+     */
+    internal class _2769FindTheMaximumAchievableNumber
+    {
+        public int TheMaximumAchievableX(int num, int t)
+        {
+
+            int newNum = num + t;
+            int x = newNum + t;
+            return x;
+        }
+    }
+}
