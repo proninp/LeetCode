93e2b717d8e966b3b866d207515f9e7507dcbe65
proninp <proninpaul@yandex.ru>
2023-08-09 08:27:59 +0300

    C# 2413. Smallest Even Multiple

---
+++ C Sharp/LeetCode/Easy/2413SmallestEvenMultiple.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCode.Easy
+{
+    /*
+     * 2413. Smallest Even Multiple
+     * Given a positive integer n, return the smallest positive integer that is a multiple of both 2 and n.
+     */
+    internal class _2413SmallestEvenMultiple
+    {
+        public int SmallestEvenMultiple(int n) => (n % 2 == 0) ? n : n * 2;
+    }
+}
