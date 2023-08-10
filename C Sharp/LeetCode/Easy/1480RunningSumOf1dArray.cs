7a82a6c0c5fea4a478a9d90fea7056d236a0c68f
proninp <proninpaul@yandex.ru>
2023-08-10 08:32:41 +0300

    C# 1480. Running Sum of 1d Array

---
+++ C Sharp/LeetCode/Easy/1480RunningSumOf1dArray.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Easy
+{
+    /*
+     * 1480. Running Sum of 1d Array
+     * Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+     * Return the running sum of nums.
+     */
+    internal class _1480RunningSumOf1dArray
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            var output = new int[nums.Length];
+            output[0] = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+                output[i] += nums[i] + output[i - 1];
+            return output;
+        }
+    }
+}
