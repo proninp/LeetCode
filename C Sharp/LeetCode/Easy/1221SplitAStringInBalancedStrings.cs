8c09767b96698b58655a9dda9aaa6372b8355275
proninp <proninpaul@yandex.ru>
2023-08-11 08:20:19 +0300

    C# 1221. Split a String in Balanced Strings

---
+++ C Sharp/LeetCode/Easy/1221SplitAStringInBalancedStrings.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Easy
+{
+    internal class _1221SplitAStringInBalancedStrings
+    {
+        /*
+         * 1221. Split a String in Balanced Strings
+         * Balanced strings are those that have an equal quantity of 'L' and 'R' characters.
+         * Given a balanced string s, split it into some number of substrings such that:
+         * Each substring is balanced.
+         * Return the maximum number of balanced strings you can obtain.
+         */
+        public int BalancedStringSplit(string s)
+        {
+            var output = 0;
+            var balance = 0;
+            foreach (var c in s)
+            {
+                if (c == 'R')
+                    balance++;
+                else if (c == 'L')
+                    balance--;
+                if (balance == 0)
+                    output++;
+            }
+            return output;
+        }
+    }
+}
