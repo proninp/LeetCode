a0ae32f735b98b8bfebd8994e7ecb62476d87464
proninp <proninpaul@yandex.ru>
2023-08-14 14:49:08 +0300

    C# 2235. Add Two Integers

---
+++ C Sharp/LeetCode/Easy/2235AddTwoIntegers.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCode.Easy
+{
+    /*
+     * 2235. Add Two Integers
+     * Given two integers num1 and num2, return the sum of the two integers.
+     */
+    internal class _2235AddTwoIntegers
+    {
+        public int Sum(int num1, int num2) => num1 + num2;
+    }
+}
