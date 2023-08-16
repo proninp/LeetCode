b5098416192a3f9ee0a042ed3a912f2646bc0531
proninp <proninpaul@yandex.ru>
2023-08-16 14:26:01 +0300

    C# 771. Jewels and Stones

---
+++ C Sharp/LeetCode/Easy/771JewelsAndStones.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace LeetCode.Easy
+{
+    /*
+     * 771. Jewels and Stones
+     * You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have.
+     * Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+     * Letters are case sensitive, so "a" is considered a different type of stone from "A".
+     */
+    internal class _771JewelsAndStones
+    {
+        public int NumJewelsInStones(string jewels, string stones)
+        {
+            return stones.Count(stone => jewels.Contains(stone));
+        }
+    }
+}
