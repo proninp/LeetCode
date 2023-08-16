33ed355e0964a77202b844b834fb545578e6b0ae
proninp <proninpaul@yandex.ru>
2023-08-16 10:11:23 +0300

    SQL 1587. Bank Account Summary II

---
+++ SQL/Easy/1587. Bank Account Summary II.sql
@@ -0,0 +1,38 @@
+/*
+ * 1587. Bank Account Summary II
+ * Table: Users
+
+ * +--------------+---------+
+ * | Column Name  | Type    |
+ * +--------------+---------+
+ * | account      | int     |
+ * | name         | varchar |
+ * +--------------+---------+
+ * account is the primary key (column with unique values) for this table.
+ * Each row of this table contains the account number of each user in the bank.
+ * There will be no two users having the same name in the table.
+
+ * Table: Transactions
+ * +---------------+---------+
+ * | Column Name   | Type    |
+ * +---------------+---------+
+ * | trans_id      | int     |
+ * | account       | int     |
+ * | amount        | int     |
+ * | transacted_on | date    |
+ * +---------------+---------+
+ * trans_id is the primary key (column with unique values) for this table.
+ * Each row of this table contains all changes made to all accounts.
+ * amount is positive if the user received money and negative if they transferred money.
+ * All accounts start with a balance of 0.
+
+ * Write a solution to report the name and balance of users with a balance higher than 10000.
+ * The balance of an account is equal to the sum of the amounts of all transactions involving that account.
+ * Return the result table in any order.
+ */
+SELECT u.name
+       ,SUM(t.amount) balance
+FROM Transactions t
+LEFT JOIN Users u ON t.account = u.account
+GROUP BY u.name
+HAVING SUM(t.amount) > 10000
