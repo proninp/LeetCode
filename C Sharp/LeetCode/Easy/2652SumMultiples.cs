d73369105ffc50f858407bc6b648757966228d2a
proninp <proninpaul@yandex.ru>
2023-08-19 10:38:31 +0300

    C# 2652. Sum Multiples

---
+++ C Sharp/LeetCode/Easy/2652SumMultiples.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Easy
+{
+    /*
+     * 2652. Sum Multiples
+     * Given a positive integer n, find the sum of all integers in the range [1, n] inclusive that are divisible by 3, 5, or 7.
+     * Return an integer denoting the sum of all numbers in the given range satisfying the constraint.
+     */
+    internal class _2652SumMultiples
+    {
+        public int SumOfMultiples(int n)
+        {
+            int sum = 0;
+            for (int i = 3; i <= n; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0)
+                    sum += i;
+            }
+            return sum;
+        }
+    }
+}
