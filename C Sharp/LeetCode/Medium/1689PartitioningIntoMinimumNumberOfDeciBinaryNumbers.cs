23df4dee2920e795727143ecbe5f32efa03257de
proninp <proninpaul@yandex.ru>
2023-08-21 11:30:56 +0300

    C# * 1689. Partitioning Into Minimum Number Of Deci-Binary Numbers

---
+++ C Sharp/LeetCode/Medium/1689PartitioningIntoMinimumNumberOfDeciBinaryNumbers.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Medium
+{
+    /*
+     * 1689. Partitioning Into Minimum Number Of Deci-Binary Numbers
+     * A decimal number is called deci-binary if each of its digits is either 0 or 1 without any leading zeros.
+     * For example, 101 and 1100 are deci-binary, while 112 and 3001 are not.
+     * Given a string n that represents a positive decimal integer,
+     * return the minimum number of positive deci-binary numbers needed so that they sum up to n.
+     */
+    internal class _1689PartitioningIntoMinimumNumberOfDeciBinaryNumbers
+    {
+        public int MinPartitions(string n)
+        {
+            int max = n[0];
+            for (int i = 1; i < n.Length; i++)
+                if (n[i] > max)
+                    max = n[i];
+            return max - '0';
+        }
+    }
+}
