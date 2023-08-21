afa1028a54b77e92d88293b0ad33a00e9398b398
proninp <proninpaul@yandex.ru>
2023-08-21 11:16:04 +0300

    C# * 2824. Count Pairs Whose Sum is Less than Target

---
+++ C Sharp/LeetCode/Easy/2824CountPairsWhoseSumIsLessThanTarget.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Easy
+{
+    /*
+     * 2824. Count Pairs Whose Sum is Less than Target
+     * Given a 0-indexed integer array nums of length n and an integer target,
+     * return the number of pairs (i, j) where 0 <= i < j < n and nums[i] + nums[j] < target.
+     */
+    internal class _2824CountPairsWhoseSumIsLessThanTarget
+    {
+        public int CountPairs(IList<int> nums, int target)
+        {
+            int len = nums.Count;
+            int res = 0;
+            for (int i = 0; i < len - 1; i++)
+            {
+                for (int j = i + 1; j < len; j++)
+                {
+                    if (nums[i] + nums[j] < target)
+                        res++;
+                }
+            }
+            return res;
+        }
+    }
+}
