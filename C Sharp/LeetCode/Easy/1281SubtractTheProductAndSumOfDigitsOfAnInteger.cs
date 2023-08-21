f5e190e5bc98e300dac4b3797e122e34e4baaba6
proninp <proninpaul@yandex.ru>
2023-08-21 11:04:48 +0300

    C# 1281. Subtract the Product and Sum of Digits of an Integer

---
+++ C Sharp/LeetCode/Easy/1281SubtractTheProductAndSumOfDigitsOfAnInteger.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Easy
+{
+    /*
+     * 1281. Subtract the Product and Sum of Digits of an Integer
+     * Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+     */
+    internal class _1281SubtractTheProductAndSumOfDigitsOfAnInteger
+    {
+        public int SubtractProductAndSum(int n)
+        {
+            int sum = 0;
+            int mult = 1;
+            int temp;
+            while (n != 0)
+            {
+                temp = n % 10;
+                n /= 10;
+                sum += temp;
+                mult *= temp;
+            }
+            return mult - sum;
+        }
+    }
+}
