673eeda924a74cfcd4dff299f00b1e31825ee123
proninp <proninpaul@yandex.ru>
2023-08-25 08:08:16 +0300

    C# 876. Middle of the Linked List

---
+++ C Sharp/LeetCode/Easy/876MiddleOfTheLinkedList.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Common;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Easy
+{
+    /*
+     * 876. Middle of the Linked List
+     * Given the head of a singly linked list, return the middle node of the linked list.
+     * If there are two middle nodes, return the second middle node.
+     */
+    internal class _876MiddleOfTheLinkedList
+    {
+        public ListNode MiddleNode(ListNode head)
+        {
+            var list = new List<ListNode>();
+            while (head != null)
+            {
+                list.Add(head);
+                head = head.next;
+            }
+            return list.ElementAt(list.Count / 2);
+        }
+    }
+}
