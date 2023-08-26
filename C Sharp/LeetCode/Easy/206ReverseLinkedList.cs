d2123edb28bb8d564a7a44b898bb8e088f18cf8e
proninp <proninpaul@yandex.ru>
2023-08-26 15:42:32 +0300

    C# 206. Reverse Linked List

---
+++ C Sharp/LeetCode/Easy/206ReverseLinkedList.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Easy
+{
+    /*
+     * 206. Reverse Linked List
+     * Given the head of a singly linked list, reverse the list, and return the reversed list.
+     */
+    internal class _206ReverseLinkedList
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode tail = null;
+            ListNode next;
+            while (head != null)
+            {
+                next = head.next;
+                head.next = tail;
+                tail = head;
+                head = next;
+            }
+            return tail;
+        }
+
+    }
+}
