a1c2da1a6f5f353043654659db66fbbef627da66
proninp <proninpaul@yandex.ru>
2023-08-28 17:11:30 +0300

    C# 203. Remove Linked List Elements

---
+++ C Sharp/LeetCode/Easy/203RemoveLinkedListElements.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Easy
+{
+    /*
+     * 203. Remove Linked List Elements
+     * Given the head of a linked list and an integer val,
+     * remove all the nodes of the linked list that has Node.val == val, and return the new head.
+     */
+    public class _203RemoveLinkedListElements
+    {
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+            var root = new ListNode(0, head);
+            var node = root;
+            while (node != null && node.next != null)
+            {
+                if (node.next.val == val)
+                    node.next = node.next.next;
+                else
+                    node = node.next;
+            }
+            return root.next;
+        }
+    }
+}
