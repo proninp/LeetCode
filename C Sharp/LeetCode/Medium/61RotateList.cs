225b767daacb9679666ed448de694369c60f291b
proninp <proninpaul@yandex.ru>
2023-09-12 07:49:44 +0300

    C# 61. Rotate List

---
+++ C Sharp/LeetCode/Medium/61RotateList.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Medium
+{
+    /*
+     * 61. Rotate List
+     * Given the head of a linked list, rotate the list to the right by k places.
+     */
+    public class _61RotateList
+    {
+        public ListNode RotateRight(ListNode head, int k)
+        {
+            if (head == null)
+                return head;
+            int size = 1;
+            var tail = head;
+            while (tail.next != null)
+            {
+                tail = tail.next;
+                size++;
+            }
+            k %= size;
+            if (k == 0)
+                return head;
+            var current = head;
+            int i = size - k - 1;
+            while (i-- > 0)
+                current = current.next;
+            var newHead = current.next;
+            current.next = null;
+            tail.next = head;
+            return newHead;
+        }
+    }
+}
