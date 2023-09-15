8353bfc719c5cb453e1d40cc51118bac346ddc60
proninp <proninpaul@yandex.ru>
2023-09-15 11:11:00 +0300

    C# 121. Best Time to Buy and Sell Stock

---
+++ C Sharp/LeetCode/Easy/121BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LeetCode.Easy
+{
+    /*
+     * 121. Best Time to Buy and Sell Stock
+     * You are given an array prices where prices[i] is the price of a given stock on the ith day.
+     * You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+     * Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+     */
+    public class _121BestTimeToBuyAndSellStock
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int l = 0, r = 1;
+            int profit, maxProfit = 0;
+            while (r < prices.Length)
+            {
+                if (prices[l] < prices[r])
+                {
+                    profit = prices[r] - prices[l];
+                    maxProfit = Math.Max(maxProfit, profit);
+                }
+                else
+                    l = r;
+                r++;
+            }
+            return maxProfit;
+        }
+    }
+}
