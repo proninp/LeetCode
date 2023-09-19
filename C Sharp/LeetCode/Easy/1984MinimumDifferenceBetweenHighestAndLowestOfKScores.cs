1ac8d57799e83f1f321c1dcd2853bfb12d35812c
proninp <proninpaul@yandex.ru>
2023-09-19 08:16:01 +0300

    C# 1984. Minimum Difference Between Highest and Lowest of K Scores

---
+++ C Sharp/LeetCode/Easy/1984MinimumDifferenceBetweenHighestAndLowestOfKScores.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LeetCode.Easy
+{
+    public class _1984MinimumDifferenceBetweenHighestAndLowestOfKScores
+    {
+        /*
+         * 1984. Minimum Difference Between Highest and Lowest of K Scores
+         * You are given a 0-indexed integer array nums, where nums[i] represents the score of the ith student.
+         * You are also given an integer k.
+         * Pick the scores of any k students from the array so that the difference
+         * between the highest and the lowest of the k scores is minimized.
+         * Return the minimum possible difference.
+         */
+        public int MinimumDifference(int[] nums, int k)
+        {
+            Array.Sort(nums);
+            int min = int.MaxValue;
+            int l = 0, r = k - 1;
+            while (r < nums.Length)
+                min = Math.Min(min, nums[r++] - nums[l++]);
+            return min;
+        }
+    }
+}
