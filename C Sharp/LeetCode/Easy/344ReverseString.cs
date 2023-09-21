995952dddc267ea74513452aaa4c26025a9d6606
proninp <proninpaul@yandex.ru>
2023-09-21 07:37:52 +0300

    C# 344. Reverse String

---
+++ C Sharp/LeetCode/Easy/344ReverseString.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Easy
+{
+    /*
+     * 344. Reverse String
+     * Write a function that reverses a string. The input string is given as an array of characters s.
+     * You must do this by modifying the input array in-place with O(1) extra memory.
+     */
+    public class _344ReverseString
+    {
+        public void ReverseString(char[] s)
+        {
+            int l = 0, r = s.Length - 1;
+            while (l < r)
+            {
+                (s[l], s[r]) = (s[r], s[l]);
+                (l, r) = (l + 1, r - 1);
+            }
+
+        }
+    }
+}
