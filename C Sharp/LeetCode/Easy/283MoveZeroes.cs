4afbedab92f7dcb00ef41aaecb14f263f95fa2b2
proninp <proninpaul@yandex.ru>
2023-09-22 09:05:36 +0300

    C# 283. Move Zeroes

---
+++ C Sharp/LeetCode/Easy/283MoveZeroes.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Easy
+{
+    /*
+     * 283. Move Zeroes
+     * Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+     * Note that you must do this in-place without making a copy of the array.
+     */
+    public class _283MoveZeroes
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            int l = 0;
+            for (int r = 0; r < nums.Length; r++)
+            {
+                if (nums[r] != 0)
+                {
+                    (nums[l], nums[r]) = (nums[r], nums[l]);
+                    l++;
+                }
+            }
+        }
+    }
+}
