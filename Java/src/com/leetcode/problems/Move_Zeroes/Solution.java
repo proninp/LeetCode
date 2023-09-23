5fe433694a75bf62580155dbee0b6e64c1207efc
proninp <proninpaul@yandex.ru>
2023-09-23 10:59:53 +0300

    Java 283. Move Zeroes

---
+++ Java/src/com/leetcode/problems/Move_Zeroes/Solution.java
@@ -0,0 +1,15 @@
+package com.leetcode.problems.Move_Zeroes;
+
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        int l = 0;
+        for (int r = 0; r < nums.length; r++) {
+            if (nums[r] != 0) {
+                int t = nums[r];
+                nums[r] = nums[l];
+                nums[l] = t;
+                l++;
+            }
+        }
+    }
+}
