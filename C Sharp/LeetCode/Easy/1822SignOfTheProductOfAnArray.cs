509eab166200c89974050c45cc84f10ae808cd21
proninp <proninpaul@yandex.ru>
2023-09-26 08:53:06 +0300

    C# 1822. Sign of the Product of an Array

---
+++ C Sharp/LeetCode/Easy/1822SignOfTheProductOfAnArray.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Easy
+{
+    /*
+     * 1822. Sign of the Product of an Array
+     * 
+     * There is a function signFunc(x) that returns:
+     * 1 if x is positive.
+     * -1 if x is negative.
+     * 0 if x is equal to 0.
+     * You are given an integer array nums. Let product be the product of all values in the array nums.
+     * Return signFunc(product).
+     */
+    public class _1822SignOfTheProductOfAnArray
+    {
+        public int ArraySign(int[] nums)
+        {
+            int lz = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] < 0)
+                    lz++;
+                else if (nums[i] == 0)
+                    return 0;
+            }
+            return lz % 2 != 0 ? -1 : 1;
+        }
+    }
+}
