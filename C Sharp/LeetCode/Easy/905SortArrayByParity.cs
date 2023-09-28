dd266b6ef7fe3a1814117f2a67f3db6487438431
proninp <proninpaul@yandex.ru>
2023-09-28 19:13:14 +0300

    C# 905. Sort Array By Parity

---
+++ C Sharp/LeetCode/Easy/905SortArrayByParity.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Easy
+{
+    /*
+     * 905. Sort Array By Parity
+     * Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+     * Return any array that satisfies this condition.
+     */
+    public class _905SortArrayByParity
+    {
+        public int[] SortArrayByParity(int[] nums)
+        {
+            int l = 0;
+            for (int r = 0; r < nums.Length; r++)
+            {
+                if (nums[r] % 2 == 0)
+                {
+                    (nums[l], nums[r]) = (nums[r], nums[l]);
+                    l++;
+                }
+            }
+            return nums;
+        }
+    }
+}
