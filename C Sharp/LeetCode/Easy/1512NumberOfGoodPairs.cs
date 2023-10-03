d035cef27ef40c8176c120b320b1f95fa6a147f7
proninp <proninpaul@yandex.ru>
2023-10-03 08:45:34 +0300

    C# 1512. Number of Good Pairs

--- C Sharp/LeetCode/Easy/1512NumberOfGoodPairs.cs
+++ C Sharp/LeetCode/Easy/1512NumberOfGoodPairs.cs
@@ -8,11 +8,27 @@ namespace LeetCode.Easy
 {
     /*
      * 1512. Number of Good Pairs
+     * 
      * Given an array of integers nums, return the number of good pairs.
      * A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+     * 
+     * Constraints:
+     * 1 <= nums.length <= 100
+     * 1 <= nums[i] <= 100
      */
     internal class _1512NumberOfGoodPairs
     {
+        public static int NumIdenticalPairs2(int[] nums)
+        {
+            var counts = new int[102];
+            foreach(var e in nums)
+                counts[e]++;
+            int ans = 0;
+            foreach(var e in counts)
+                ans += (e * (e - 1)) / 2;
+            return ans;
+        }
+
         public static int NumIdenticalPairs(int[] nums)
         {
             Dictionary<int, int> map = new Dictionary<int, int>();
