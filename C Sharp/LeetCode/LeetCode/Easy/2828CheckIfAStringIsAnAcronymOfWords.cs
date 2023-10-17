bc91fdf8e9751c6d49c25efca1cddd2dd715fd1e
proninp <proninpaul@yandex.ru>
2023-10-17 23:44:52 +0300

    C# 2828. Check if a String Is an Acronym of Words

---
+++ C Sharp/LeetCode/LeetCode/Easy/2828CheckIfAStringIsAnAcronymOfWords.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Easy
+{
+    /*
+     * 2828. Check if a String Is an Acronym of Words
+     * 
+     * Given an array of strings words and a string s, determine if s is an acronym of words.
+     * The string s is considered an acronym of words if it can be formed by concatenating the first character of each string in words in order.
+     * For example, "ab" can be formed from ["apple", "banana"], but it can't be formed from ["bear", "aardvark"].
+     * Return true if s is an acronym of words, and false otherwise.
+     */
+    public class _2828CheckIfAStringIsAnAcronymOfWords
+    {
+        public bool IsAcronym(IList<string> words, string s)
+        {
+            return string.Concat(words.Select(x => x[0])).Equals(s);
+        }
+    }
+}
