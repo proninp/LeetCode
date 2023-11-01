6d7f338c737a2f924600936ba77faedbe1ccca7e
proninp <proninpaul@yandex.ru>
2023-11-01 19:59:06 +1100

    SQL 1050. Actors and Directors Who Cooperated

---
+++ SQL/Easy/1050. Actors and Directors Who Cooperated At Least Three Times.sql
@@ -0,0 +1,22 @@
+/*
+ * 1050. Actors and Directors Who Cooperated At Least Three Times
+ * 
+ * Table: ActorDirector
+ * 
+ * +-------------+---------+
+ * | Column Name | Type    |
+ * +-------------+---------+
+ * | actor_id    | int     |
+ * | director_id | int     |
+ * | timestamp   | int     |
+ * +-------------+---------+
+ * timestamp is the primary key (column with unique values) for this table.
+ * Write a solution to find all the pairs (actor_id, director_id) where the actor has cooperated with the director at least three times.
+ * Return the result table in any order.
+ */
+SELECT actor_id
+       , director_id
+FROM ActorDirector
+GROUP BY actor_id
+         , director_id
+         HAVING COUNT(*) > 2
