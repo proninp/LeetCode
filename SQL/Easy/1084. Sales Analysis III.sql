49f4e88dfe468c622891370e8d43b75c4bedf7d9
proninp <proninpaul@yandex.ru>
2023-11-02 22:09:10 +1100

    SQL 1084. Sales Analysis III

---
+++ SQL/Easy/1084. Sales Analysis III.sql
@@ -0,0 +1,39 @@
+/*
+ * 1084. Sales Analysis III
+ * Table: Product
+ * 
+ * +--------------+---------+
+ * | Column Name  | Type    |
+ * +--------------+---------+
+ * | product_id   | int     |
+ * | product_name | varchar |
+ * | unit_price   | int     |
+ * +--------------+---------+
+ * product_id is the primary key (column with unique values) of this table.
+ * Each row of this table indicates the name and the price of each product.
+ * Table: Sales
+ * 
+ * +-------------+---------+
+ * | Column Name | Type    |
+ * +-------------+---------+
+ * | seller_id   | int     |
+ * | product_id  | int     |
+ * | buyer_id    | int     |
+ * | sale_date   | date    |
+ * | quantity    | int     |
+ * | price       | int     |
+ * +-------------+---------+
+ * This table can have duplicate rows.
+ * product_id is a foreign key (reference column) to the Product table.
+ * Each row of this table contains some information about one sale. 
+ * Write a solution to report the products that were only sold in the first quarter of 2019.
+ * That is, between 2019-01-01 and 2019-03-31 inclusive.
+ * Return the result table in any order.
+*/
+SELECT p.product_id
+       , p.product_name
+FROM Product p
+LEFT JOIN Sales s ON p.product_id = s.product_id
+GROUP BY p.product_id
+         , p.product_name
+HAVING MIN(s.sale_date) > '2018-12-31' AND MAX(sale_date) < '2019-04-01'
