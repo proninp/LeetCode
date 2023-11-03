a77efbf2be512ac788f069f576706fc0ed4b2c78
proninp <proninpaul@yandex.ru>
2023-11-03 15:54:55 +1100

    SQL 1158. Market Analysis I

---
+++ SQL/Medium/1158. Market Analysis I.sql
@@ -0,0 +1,49 @@
+/*
+ * 1158. Market Analysis I
+ * Table: Users
+ * 
+ * +----------------+---------+
+ * | Column Name    | Type    |
+ * +----------------+---------+
+ * | user_id        | int     |
+ * | join_date      | date    |
+ * | favorite_brand | varchar |
+ * +----------------+---------+
+ * user_id is the primary key (column with unique values) of this table.
+ * This table has the info of the users of an online shopping website where users can sell and buy items. 
+ * 
+ * Table: Orders
+ * 
+ * +---------------+---------+
+ * | Column Name   | Type    |
+ * +---------------+---------+
+ * | order_id      | int     |
+ * | order_date    | date    |
+ * | item_id       | int     |
+ * | buyer_id      | int     |
+ * | seller_id     | int     |
+ * +---------------+---------+
+ * order_id is the primary key (column with unique values) of this table.
+ * item_id is a foreign key (reference column) to the Items table.
+ * buyer_id and seller_id are foreign keys to the Users table. 
+ * 
+ * Table: Items
+ * 
+ * +---------------+---------+
+ * | Column Name   | Type    |
+ * +---------------+---------+
+ * | item_id       | int     |
+ * | item_brand    | varchar |
+ * +---------------+---------+
+ * item_id is the primary key (column with unique values) of this table. 
+ * 
+ * Write a solution to find for each user, the join date and the number of orders they made as a buyer in 2019.
+ * 
+ * Return the result table in any order.
+*/
+SELECT u.user_id buyer_id
+       ,  MIN(u.join_date) join_date
+       , COUNT(o.order_id) orders_in_2019
+FROM Users u 
+LEFT JOIN Orders o ON u.user_id = o.buyer_id AND o.order_date BETWEEN '2019-01-01' AND '2019-12-31'
+GROUP BY u.user_id
