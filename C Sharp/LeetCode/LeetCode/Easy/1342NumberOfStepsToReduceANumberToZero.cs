42e4030f8b7b8657465c93b766e122c598fdf80f
proninp <proninpaul@yandex.ru>
2023-11-09 23:07:16 +1100

    C# 1342. Number of Steps to Reduce a Number to Zero

---
+++ C Sharp/LeetCode/LeetCode/Easy/1342NumberOfStepsToReduceANumberToZero.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Easy
+{
+    /*
+     * 1342. Number of Steps to Reduce a Number to Zero
+     * Given an integer num, return the number of steps to reduce it to zero.
+     * In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+     */
+    public class _1342NumberOfStepsToReduceANumberToZero
+    {
+        public int NumberOfSteps(int num)
+        {
+            var output = 0;
+            while(num > 0)
+            {
+                if (num % 2 == 0)
+                    num /= 2;
+                else
+                    num -= 1;
+                output++;
+            }
+            return output;
+        }
+    }
+}
