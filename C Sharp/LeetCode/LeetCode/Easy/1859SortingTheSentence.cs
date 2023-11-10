497a81525837c2090f5152a3d3d32fbeacb2f71c
proninp <proninpaul@yandex.ru>
2023-11-10 22:56:37 +1100

    C# 1859. Sorting the Sentence

---
+++ C Sharp/LeetCode/LeetCode/Easy/1859SortingTheSentence.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Easy
+{
+    /*
+     * 1859. Sorting the Sentence
+     */
+    public class _1859SortingTheSentence
+    {
+        public string SortSentence(string s)
+        {
+            var array = s.Split(' ');
+            var res = new string[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                var idx = array[i][array[i].Length - 1] + '0' - 96 - 1;
+                res[idx] = array[i].Substring(0, array[i].Length - 1);
+            }
+            return string.Join(' ', res);
+        }
+    }
+}
