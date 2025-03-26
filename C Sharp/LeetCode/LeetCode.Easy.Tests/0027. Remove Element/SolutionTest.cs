9ba7e7a2a50ec2e25e3b1e1708f25b1d747b94f5
proninp <proninpaul@yandex.ru>
2025-03-26 09:17:22 +0300

    Easy - 27. Remove Element

---
+++ C Sharp/LeetCode/LeetCode.Easy.Tests/0027. Remove Element/SolutionTest.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Easy._0027._Remove_Element.src;
+
+namespace LeetCode.Easy.Tests._0027._Remove_Element;
+
+public class SolutionTest
+{
+    [Theory]
+    [InlineData(new int[] { 3, 2, 2, 3 }, 3, new int[] { 2, 2 })]
+    [InlineData(new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2, new int[] { 0, 1, 3, 0, 4 })]
+    [InlineData(new int[] { 1, 2 }, 3, new int[] { 1, 2 })]
+    [InlineData(new int[] { 2 }, 3, new int[] { 2 })]
+    public void BasicTests(int[] actual, int val, int[] expected)
+    {
+        // Arrange
+
+        // Act
+        var result = Solution.RemoveElement(actual, val);
+
+        // Assert
+        Assert.Equal(expected, actual.Take(result).ToArray());
+    }
+}
+
---
+++ C Sharp/LeetCode/LeetCode.Easy/0027. Remove Element/src/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Easy._0027._Remove_Element.src;
+
+public class Solution
+{
+    public static int RemoveElement(int[] nums, int val)
+    {
+        int l = 0;
+        for (int r = 0; r < nums.Length; r++)
+        {
+            if (nums[r] != val)
+            {
+                nums[l] = nums[r];
+                l++;
+            }
+        }
+        return l;
+    }
+}
