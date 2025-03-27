b04689faf1dfc6da800ce3e4c699a11b9b82f7e7
proninp <proninpaul@yandex.ru>
2025-03-27 16:35:50 +0300

    Easy - 35. Search Insert Position

---
+++ C Sharp/LeetCode/LeetCode.Easy/35. Search Insert Position/src/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Easy._35._Search_Insert_Position.src;
+
+public class Solution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        if (nums[0] > target)
+            return 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] >= target)
+                return i;
+        }
+        return nums.Length;
+    }
+}
