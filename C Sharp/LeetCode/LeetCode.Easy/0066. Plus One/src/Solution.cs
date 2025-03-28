29b69247d64abc0a23550b2b5c0dee93994be1e0
proninp <proninpaul@yandex.ru>
2025-03-28 09:07:35 +0300

    Easy - 66. Plus One

---
+++ C Sharp/LeetCode/LeetCode.Easy/0066. Plus One/src/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Easy._0066._Plus_One.src;
+
+public sealed class Solution
+{
+    public int[] PlusOne(int[] digits)
+    {
+        var add = 1;
+        var list = new List<int>(digits.Length);
+        for (int i = digits.Length - 1;i >= 0; i--)
+        {
+            var d = digits[i] + add;
+            add = 0;
+            if (d > 9)
+            {
+                d -= 10;
+                add++;
+            }
+            list.Add(d);
+        }
+        if (add > 0)
+        {
+            list.Add(add);
+        }
+        list.Reverse();
+        return list.ToArray();
+    }
+}
+
