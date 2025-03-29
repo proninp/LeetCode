5c58be2f596bc18a78d3640b593853bd09dbe4f2
proninp <proninpaul@yandex.ru>
2025-03-29 09:50:24 +0300

    Easy - 88. Merge Sorted Array

---
+++ C Sharp/LeetCode/LeetCode.Easy/0088. Merge Sorted Array/src/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Easy._0088._Merge_Sorted_Array.src;
+
+public sealed class Solution
+{
+    public static void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        var last = nums1.Length - 1;
+        int p1 = m - 1, p2 = n - 1;
+        while (n > 0)
+        {
+            if (m > 0 && nums1[p1] > nums2[p2])
+            {
+                nums1[last] = nums1[p1--];
+            }
+            else
+            {
+                nums1[last] = nums2[p2--];
+            }
+            last--;
+        }
+    }
+}
+
