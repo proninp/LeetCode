052c16acd1ec938147feca94beed8aa0dd112618
proninp <proninpaul@yandex.ru>
2025-03-30 10:53:27 +0300

    Easy - 118. Pascal's Triangle

---
+++ C Sharp/LeetCode/LeetCode.Easy/0118. Pascal's Triangle/README.md
@@ -0,0 +1,17 @@
+# 118. Pascal's Triangle
+
+Given an integer `numRows`, return the first numRows of __[Pascal's triangle](https://en.wikipedia.org/wiki/Pascal%27s_triangle)__.
+
+In __Pascal's triangle__, each number is the sum of the two numbers directly above it.
+
+__Example 1:__
+```
+Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+```
+
+__Example 2:__
+```
+Input: numRows = 1
+Output: [[1]]
+```
---
+++ C Sharp/LeetCode/LeetCode.Easy/0118. Pascal's Triangle/src/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Easy._0118._Pascal_s_Triangle.src;
+
+public sealed class Solution
+{
+    public static IList<IList<int>> Generate(int numRows)
+    {
+        var result = new int[numRows][];
+        for (int i = 0; i < numRows; i++)
+        {
+            result[i] = new int[i + 1];
+            for (int j = 0; j <= i; j++)
+            {
+                if (j == 0 || j == i)
+                {
+                    result[i][j] = 1;
+                }
+                else
+                {
+                    result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
+                }
+            }
+        }
+        return result;
+    }
+}
