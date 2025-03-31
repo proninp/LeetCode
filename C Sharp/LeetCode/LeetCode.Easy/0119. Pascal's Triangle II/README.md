8714ab614f8df2c7f75dbd79ef69baa3f64590eb
proninp <proninpaul@yandex.ru>
2025-03-31 08:56:40 +0300

    Easy - 119. Pascal's Triangle II

---
+++ C Sharp/LeetCode/LeetCode.Easy/0119. Pascal's Triangle II/README.md
@@ -0,0 +1,24 @@
+# 119. Pascal's Triangle II
+
+Given an integer `rowIndex`, return the `rowIndex<sup>th</sup>` (__0-indexed__) row of the __[Pascal's triangle](https://en.wikipedia.org/wiki/Pascal%27s_triangle)__.
+
+In __Pascal's triangle__, each number is the sum of the two numbers directly above it.
+
+
+__Example 1:__
+```
+Input: rowIndex = 3
+Output: [1,3,3,1]
+```
+
+__Example 2:__
+```
+Input: rowIndex = 0
+Output: [1]
+```
+
+__Example 3:__
+```
+Input: rowIndex = 1
+Output: [1,1]
+```
---
+++ C Sharp/LeetCode/LeetCode.Easy/0119. Pascal's Triangle II/src/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy._0119._Pascal_s_Triangle_II.src;
+
+public sealed class Solution
+{
+    public IList<int> GetRow(int rowIndex)
+    {
+        var triangle = new int[rowIndex + 1][];
+        for (int i = 0; i < triangle.Length; i++)
+        {
+            triangle[i] = new int[i + 1];
+            for (int j = 0; j <= i; j++)
+            {
+                if (j == 0 || j == i)
+                {
+                    triangle[i][j] = 1;
+                }
+                else
+                {
+                    triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+                }
+            }
+        }
+        return triangle[rowIndex];
+    }
+}
+
