894a39ecba91ceda2994a46bc4d90761e009bf8b
proninp <proninpaul@yandex.ru>
2025-04-02 09:20:56 +0300

    Easy - 121. Best Time to Buy and Sell Stock

---
+++ C Sharp/LeetCode/LeetCode.Easy.Tests/0121. Best Time to Buy and Sell Stock/SolutionTest.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode.Easy._0121._Best_Time_to_Buy_and_Sell_Stock.src;
+
+namespace LeetCode.Easy.Tests._0121._Best_Time_to_Buy_and_Sell_Stock;
+
+public class SolutionTest
+{
+    [Theory]
+    [InlineData(new int[] { 7, 1, 5, 3, 6, 4 }, 5)]
+    [InlineData(new int[] { 7, 6, 4, 3, 1 }, 0)]
+    [InlineData(new int[] { 1 }, 0)]
+    public void BasicTests(int[] prices, int expected)
+    {
+        var actual = Solution.MaxProfit(prices);
+
+        Assert.Equal(expected, actual);
+    }
+}
+
---
+++ C Sharp/LeetCode/LeetCode.Easy/0121. Best Time to Buy and Sell Stock/src/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Easy._0121._Best_Time_to_Buy_and_Sell_Stock.src;
+
+public class Solution
+{
+    public static int MaxProfit(int[] prices)
+    {
+        var lowerPrice = prices[0];
+        var maxProfit = 0;
+        for (int i = 1; i < prices.Length; i++)
+        {
+            var profit = prices[i] - lowerPrice;
+            if (profit > maxProfit)
+                maxProfit = profit;
+            if (prices[i] < lowerPrice)
+                lowerPrice = prices[i];
+        }
+        return maxProfit;
+    }
+}
