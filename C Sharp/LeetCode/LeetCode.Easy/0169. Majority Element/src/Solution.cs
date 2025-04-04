7d53a19ec41d1679782942349f598c0d04ed2f41
proninp <proninpaul@yandex.ru>
2025-04-04 10:24:29 +0300

    Easy - 169. Majority Element

---
+++ C Sharp/LeetCode/LeetCode.Easy/0169. Majority Element/src/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Easy._0169._Majority_Element.src;
+
+public class Solution
+{
+    public int MajorityElement(int[] nums)
+    {
+        var mid = nums.Length / 2;
+        Dictionary<int, int> map = new();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (map.ContainsKey(nums[i]))
+                map[nums[i]]++;
+            else
+                map.Add(nums[i], 1);
+
+            if (map[nums[i]] > mid)
+                return nums[i];
+        }
+        return nums[0];
+    }
+}
+
