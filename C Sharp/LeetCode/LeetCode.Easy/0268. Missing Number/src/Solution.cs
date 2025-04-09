46cdbb4d479500d2caf4295ff4b24827a7111523
proninp <proninpaul@yandex.ru>
2025-04-09 08:09:20 +0300

    Easy - 268. Missing Number

---
+++ C Sharp/LeetCode/LeetCode.Easy/0268. Missing Number/src/Solution.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCode.Easy._0268._Missing_Number.src;
+
+public sealed class Solution
+{
+    public int MissingNumber(int[] nums)
+    {
+        var n = nums.Length;
+        var expecedSum = n * (n + 1) / 2;
+        return expecedSum - nums.Sum();
+    }
+}
