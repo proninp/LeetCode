38d5f38bd704c25e79e3e530345053aa83d23f4c
proninp <proninpaul@yandex.ru>
2025-04-14 22:42:41 +0300

    Easy - 349. Intersection of Two Arrays

---
+++ C Sharp/LeetCode/LeetCode.Easy/0349. Intersection of Two Arrays/src/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Easy._0349._Intersection_of_Two_Arrays.src;
+
+public sealed class Solution
+{
+    public int[] Intersection(int[] nums1, int[] nums2)
+    {
+        var set = new HashSet<int>();
+        var freq = new int[1001];
+
+        for (int i = 0; i < nums1.Length; i++)
+            freq[nums1[i]]++;
+
+        for (int i = 0;i < nums2.Length; i++)
+            if (freq[nums2[i]] != 0)
+                set.Add(nums2[i]);
+        return set.ToArray();
+    }
+}
