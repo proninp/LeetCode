e9b361c2edde73a767cceee38efafacc2e176fc6
proninp <proninpaul@yandex.ru>
2025-04-15 08:25:00 +0300

    Easy - 350. Intersection of Two Arrays II

---
+++ C Sharp/LeetCode/LeetCode.Easy/0350. Intersection of Two Arrays II/README.md
@@ -0,0 +1,16 @@
+# 350. Intersection of Two Arrays II
+
+Given two integer arrays `nums1` and `nums2`, return _an array of their intersection_. Each element in the result must appear as many times as it shows in both arrays and you may return the result in __any order__.
+
+__Example 1:__
+```
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+```
+
+__Example 2:__
+```
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+Explanation: [9,4] is also accepted.
+```
---
+++ C Sharp/LeetCode/LeetCode.Easy/0350. Intersection of Two Arrays II/src/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Easy._0350._Intersection_of_Two_Arrays_II.src;
+
+public sealed class Solution
+{
+    public int[] Intersect(int[] nums1, int[] nums2)
+    {
+        var result = new List<int>();
+        var freq = new int[1001];
+        for (int i = 0; i < nums1.Length; i++)
+            freq[nums1[i]]++;
+
+        for (int i = 0; i < nums2.Length; i++)
+        {
+            if (freq[nums2[i]] > 0)
+            {
+                result.Add(nums2[i]);
+                freq[nums2[i]]--;
+            }
+        }
+        return result.ToArray();
+    }
+}
