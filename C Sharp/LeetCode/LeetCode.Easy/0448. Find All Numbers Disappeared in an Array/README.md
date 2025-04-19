75e9f70f38ded199be8c474d692231be57830dc7
proninp <proninpaul@yandex.ru>
2025-04-19 10:22:38 +0300

    Easy - 448. Find All Numbers Disappeared in an Array

---
+++ C Sharp/LeetCode/LeetCode.Easy/0448. Find All Numbers Disappeared in an Array/README.md
@@ -0,0 +1,16 @@
+# 448. Find All Numbers Disappeared in an Array
+
+Given an array `nums` of `n` integers where `nums[i]` is in the range `[1, n]`, return _an array of all the integers in the range_ `[1, n]` _that do not appear in_ `nums`.
+
+
+__Example 1:__
+```
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+```
+
+__Example 2:__
+```
+Input: nums = [1,1]
+Output: [2]
+```
---
+++ C Sharp/LeetCode/LeetCode.Easy/0448. Find All Numbers Disappeared in an Array/src/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Easy._0448._Find_All_Numbers_Disappeared_in_an_Array.src;
+
+public sealed class Solution
+{
+    public IList<int> FindDisappearedNumbers(int[] nums)
+    {
+        var existsArray = new bool[nums.Length];
+        foreach (var e in nums)
+            existsArray[e - 1] = true;
+
+        var result = new List<int>();
+        for (int i = 0; i < existsArray.Length; i++)
+            if (!existsArray[i])
+                result.Add(i + 1);
+        return result;
+    }
+}
