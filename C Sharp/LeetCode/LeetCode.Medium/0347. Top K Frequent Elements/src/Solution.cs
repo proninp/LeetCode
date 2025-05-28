2b90970f6b3a7c6e2cb70362a12b5e8254d9181a
proninp <proninpaul@yandex.ru>
2025-05-28 17:53:27 +0300

    Medium - 347. Top K Frequent Elements

---
+++ C Sharp/LeetCode/LeetCode.Medium/0347. Top K Frequent Elements/src/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Medium._0347._Top_K_Frequent_Elements.src;
+
+public class Solution {
+    public int[] TopKFrequent(int[] nums, int k) {
+        var map = new Dictionary<int, int>();
+        
+        foreach (var key in nums)
+        {
+            if (!map.TryAdd(key, 1))
+                map[key]++;
+        }
+        
+        return map
+            .OrderByDescending(kvp => kvp.Value)
+            .Take(k)
+            .Select(kvp => kvp.Key)
+            .ToArray();
+    }
+}
