1de0852bbced357e60e5a936f1214b246359f527
proninp <proninpaul@yandex.ru>
2025-05-30 11:01:34 +0300

    Medium - 238. Product of Array Except Self

---
+++ C Sharp/LeetCode/LeetCode.Medium/0238. Product of Array Except Self/src/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Medium._0238._Product_of_Array_Except_Self.src;
+
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) {
+        var answer = new int[nums.Length];
+        answer[0] = 1;
+        for (var i = 1; i < nums.Length; i++)
+        {
+            answer[i] = nums[i - 1] * answer[i - 1];
+        }
+        
+        var suffix = 1;
+        for (var i = nums.Length - 1; i >= 0; i--)
+        {
+            answer[i] *= suffix;
+            suffix *= nums[i];
+        }
+        return answer;
+    }
+}
