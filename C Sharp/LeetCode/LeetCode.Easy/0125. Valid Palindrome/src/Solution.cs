63372d3ec8b060de754c6157a6af16a775c37325
proninp <proninpaul@yandex.ru>
2025-06-01 13:10:41 +0300

    Easy - 125. Valid Palindrome

---
+++ C Sharp/LeetCode/LeetCode.Easy/0125. Valid Palindrome/src/Solution.cs
@@ -0,0 +1,11 @@
+﻿using System.Text;
+
+namespace LeetCode.Easy._0125._Valid_Palindrome.src;
+
+public class Solution
+{
+    public bool IsPalindrome(string s)
+    {
+        throw new NotImplementedException();
+    }
+}
