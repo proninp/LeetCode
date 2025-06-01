3b854a0be3d3633669ddc5bb3a9ca4996bd270b0
proninp <proninpaul@yandex.ru>
2025-06-01 11:05:23 +0300

    Medium - 128. Longest Consecutive Sequence

---
+++ C Sharp/LeetCode/LeetCode.Medium/0128. Longest Consecutive Sequence/src/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Medium._0128._Longest_Consecutive_Sequence.src;
+
+public class Solution
+{
+    public int LongestConsecutive(int[] nums)
+    {
+        var set = new HashSet<int>(nums);
+        var longest = 0;
+        foreach (var e in set)
+        {
+            if (set.Contains(e - 1)) continue;
+            var currentLength = 1;
+            var num = e + 1;
+            while (set.Contains(num))
+            {
+                currentLength++;
+                num++;
+            }
+            longest = Math.Max(longest, currentLength);
+        }
+        return longest;
+    }
+}
