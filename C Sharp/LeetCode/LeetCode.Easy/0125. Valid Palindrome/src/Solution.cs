d4a6c0dd33984c2920cb9c0e8d31cee452e68581
proninp <proninpaul@yandex.ru>
2025-06-02 09:06:10 +0300

    Easy - 125. Valid Palindrome

--- C Sharp/LeetCode/LeetCode.Easy/0125. Valid Palindrome/src/Solution.cs
+++ C Sharp/LeetCode/LeetCode.Easy/0125. Valid Palindrome/src/Solution.cs
@@ -6,6 +6,19 @@ public class Solution
 {
     public bool IsPalindrome(string s)
     {
-        throw new NotImplementedException();
+        for(int left = 0, right = s.Length - 1; left < right;)
+        {
+            while (left < right && !char.IsLetterOrDigit(s[left]))
+                left++;
+
+            while (left < right && !char.IsLetterOrDigit(s[right]))
+                right--;
+            
+            if (char.ToLower(s[left]) != char.ToLower(s[right]))
+                return false;
+            left++;
+            right--;
+        }
+        return true;
     }
 }
