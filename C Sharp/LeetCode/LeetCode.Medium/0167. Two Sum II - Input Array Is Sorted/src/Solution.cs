8c005b0be06a982a790f1f4bbebc36a1e04940d0
proninp <proninpaul@yandex.ru>
2025-06-02 14:59:56 +0300

    Medium - Two Sum II - Input Array Is Sorted

---
+++ C Sharp/LeetCode/LeetCode.Medium/0167. Two Sum II - Input Array Is Sorted/src/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Medium._0167._Two_Sum_II___Input_Array_Is_Sorted.src;
+
+public class Solution
+{
+    public int[] TwoSum(int[] numbers, int target) {        
+        var i = 0;
+        var j = numbers.Length - 1;
+        while (i < j)
+        {
+            var sum = numbers[i] + numbers[j];
+            if (sum > target)
+            {
+                j--;
+            }
+            else if (sum < target)
+            {
+                i++;
+            }
+            else
+            {
+                return [i + 1, j + 1];
+            }
+        }
+        return [0];
+    }
+}
