be4a6c2861438a0bc5c1ba03d8c145fc12393b3f
proninp <proninpaul@yandex.ru>
2025-06-03 09:04:43 +0300

    Medium - 11. Container With Most Water

---
+++ C Sharp/LeetCode/LeetCode.Medium/0011. Container With Most Water/src/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Medium._0011._Container_With_Most_Water.src;
+
+public class Solution
+{
+    public int MaxArea(int[] height) {
+        var maxArea = 0;
+        var left = 0;
+        var right = height.Length - 1;
+        while (left < right)
+        {
+            maxArea = Math.Max(Math.Min(height[left], height[right]) * (right - left), maxArea);
+            if (height[left] > height[right])
+            {
+                right--;
+            }
+            else
+            {
+                left++;
+            }
+        }
+        return maxArea;
+    }
+}
