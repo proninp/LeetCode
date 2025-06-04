26aa454eea441c868f0525336255f485a3b7e423
proninp <proninpaul@yandex.ru>
2025-06-04 15:38:38 +0300

    Medium - 3. Longest Substring Without Repeating Characters

---
+++ C Sharp/LeetCode/LeetCode.Medium.Tests/0003. Longest Substring Without Repeating Characters/SolutionTest.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Medium._0003._Longest_Substring_Without_Repeating_Characters.src;
+
+namespace LeetCode.Medium.Tests._0003._Longest_Substring_Without_Repeating_Characters;
+
+public class SolutionTest
+{
+    [Theory]
+    [InlineData("abba", 2)]
+    public void TestExample1(string s, int expected)
+    {
+        var solution = new Solution();
+        var result = solution.LengthOfLongestSubstring(s);
+
+        Assert.Equal(expected, result);
+    }
+}
---
+++ C Sharp/LeetCode/LeetCode.Medium/0003. Longest Substring Without Repeating Characters/src/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Medium._0003._Longest_Substring_Without_Repeating_Characters.src;
+
+public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        var maxLength = 0;
+        var left = 0;
+        var right = 0;
+        var set = new HashSet<char>(s.Length);
+        while (right < s.Length)
+        {
+            if (!set.Add(s[right]))
+            {
+                maxLength = Math.Max(maxLength, right - left);
+                while (set.Contains(s[right]))
+                {
+                    set.Remove(s[left]);
+                    left++;
+                }
+                set.Add(s[right]);
+            }
+            right++;
+        }
+        return Math.Max(maxLength, right - left);
+    }
+}
