e27d2d639572fd2e12b98ce222acf7012b9fee44
proninp <proninpaul@yandex.ru>
2025-06-05 16:42:35 +0300

    Medium - 209. Minimum Size Subarray Sum

---
+++ C Sharp/LeetCode/LeetCode.Medium/0209. Minimum Size Subarray Sum/src/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Medium._0209._Minimum_Size_Subarray_Sum.src;
+
+public class Solution
+{
+    public int MinSubArrayLen(int target, int[] nums)
+    {
+        var minimumLength = int.MaxValue;
+        var sum = 0;
+        var l = 0;
+        
+        for (int r = 0; r < nums.Length; r++)
+        {
+            sum += nums[r];
+            while (sum >= target)
+            {
+                if (r - l + 1 < minimumLength)
+                    minimumLength = r - l + 1;
+                sum -= nums[l];
+                l++;
+            }
+        }
+        return minimumLength == int.MaxValue ?  0 : minimumLength;
+    }
+}
