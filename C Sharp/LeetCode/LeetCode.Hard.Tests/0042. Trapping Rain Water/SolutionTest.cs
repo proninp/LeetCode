423c2e4fdd748b8fb026a2d9fa835fd055f2f03a
proninp <proninpaul@yandex.ru>
2025-06-07 11:56:57 +0300

    Hard - 42. Trapping Rain Water

---
+++ C Sharp/LeetCode/LeetCode.Hard.Tests/0042. Trapping Rain Water/SolutionTest.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Hard._0042._Trapping_Rain_Water.src;
+
+namespace LeetCode.Hard.Tests._0042._Trapping_Rain_Water;
+
+public class SolutionTest
+{
+    [Theory]
+    [InlineData(new int[] { 0,1,0,2,1,0,1,3,2,1,2,1 }, 6)]
+    [InlineData(new int[] { 4,2,0,3,2,5 }, 9)]
+    public void BasicTests(int[] array, int expected)
+    {
+        var s = new Solution();
+        var actual = s.Trap(array);
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ C Sharp/LeetCode/LeetCode.Hard/0042. Trapping Rain Water/src/Solution.cs
@@ -0,0 +1,58 @@
+﻿namespace LeetCode.Hard._0042._Trapping_Rain_Water.src;
+
+public class Solution
+{
+    public int Trap(int[] height)
+    {
+        var totalVolume = 0;
+        
+        int left = 0, right = height.Length - 1;
+        var leftMax = height[0];
+        var rightMax = height[right];
+
+        while (left < right)
+        {
+            if (height[left] < height[right])
+            {
+                left++;
+                leftMax = Math.Max(leftMax, height[left]);
+                totalVolume += leftMax - height[left];
+            }
+            else
+            {
+                right--;
+                rightMax = Math.Max(rightMax, height[right]);
+                totalVolume += rightMax - height[right];
+            }
+        }
+        
+        return totalVolume;
+    }
+    
+    public int Trap2(int[] height)
+    {
+        var length = height.Length;
+        var prefixes = new int[length];
+        var suffixes = new int[length];
+
+        int maxPrefix = 0, maxSuffix = 0;
+        for (int i = 0, j = length - 1; i < length && j >= 0; i++, j--)
+        {
+            prefixes[i] = maxPrefix;
+            suffixes[j] = maxSuffix;
+            maxPrefix = Math.Max(maxPrefix, height[i]);
+            maxSuffix = Math.Max(maxSuffix, height[j]);
+        }
+
+        var totalVolume = 0;
+
+        for (var i = 0; i < length; i++)
+        {
+            var waterVolume = Math.Min(prefixes[i], suffixes[i]) - height[i];
+            if (waterVolume > 0)
+                totalVolume += waterVolume;
+        }
+        
+        return totalVolume;
+    }
+}
