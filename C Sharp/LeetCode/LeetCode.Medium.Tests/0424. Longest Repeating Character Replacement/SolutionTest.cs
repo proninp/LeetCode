f892d851d7d12d1c563115e04ace56035bbb79ab
proninp <proninpaul@yandex.ru>
2025-06-08 19:34:01 +0300

    Medium - 424. Longest Repeating Character Replacement

---
+++ C Sharp/LeetCode/LeetCode.Medium.Tests/0424. Longest Repeating Character Replacement/SolutionTest.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Medium._0424._Longest_Repeating_Character_Replacement.src;
+
+namespace LeetCode.Medium.Tests._0424._Longest_Repeating_Character_Replacement;
+
+public class SolutionTest
+{
+    [Theory]
+    [InlineData("KRSCDCSONAJNHLBMDQGIFCPEKPOHQIHLTDIQGEKLRLCQNBOHNDQGHJPNDQPERNFSSSRDEQLFPCCCARFMDLHADJADAGNNSBNCJQOF",
+        4, 7)]
+    public void BasicTests(string s, int k, int expected)
+    {
+        var solution = new Solution();
+        var actual = solution.CharacterReplacement(s, k);
+        Assert.Equal(actual, expected);
+    }
+}
---
+++ C Sharp/LeetCode/LeetCode.Medium/0424. Longest Repeating Character Replacement/src/Solution.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode.Medium._0424._Longest_Repeating_Character_Replacement.src;
+
+public class Solution
+{
+    public int CharacterReplacement(string s, int k)
+    {
+        var array = new int[26];
+        int left = 0, result = 0, maxCount = 0;
+
+        for (var right = 0; right < s.Length; right++)
+        {
+            array[s[right] - 'A']++;
+            maxCount = Math.Max(maxCount, array[s[right] - 'A']);
+            while (right - left + 1 - maxCount > k)
+            {
+                array[s[left] - 'A']--;
+                left++;
+            }
+            result = Math.Max(right - left + 1, result);
+        }
+        return result;
+    }
+    
+    public int CharacterReplacement2(string s, int k) {
+        var array = new int[26];
+        int left = 0, result = 0;
+
+        for (var right = 0; right < s.Length; right++)
+        {
+            array[s[right] - 'A']++;
+            
+            while (right - left + 1 - array.Max() > k)
+            {
+                array[s[left] - 'A']--;
+                left++;
+            }            
+            result = Math.Max(right - left + 1, result);
+        }
+            
+        return result;
+    }
+}
