840bf6c036db557e5565cd065a8c00a6ea5c3c9e
proninp <proninpaul@yandex.ru>
2025-06-09 09:59:07 +0300

    Medium - 1004. Max Consecutive Ones III

---
+++ C Sharp/LeetCode/LeetCode.Medium/1004. Max Consecutive Ones III/src/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Medium._1004._Max_Consecutive_Ones_III.src;
+
+public class Solution
+{
+    public int LongestOnes(int[] nums, int k) {
+        int left = 0, result = 0;
+        var zeroes = 0;
+        for (var right = 0; right < nums.Length; right++)
+        {
+            if (nums[right] == 0)
+                zeroes++;
+
+            while (zeroes > k)
+            {
+                if (nums[left] == 0)
+                    zeroes--;
+                left++;
+            }
+            result = Math.Max(result, right - left + 1);
+        }
+        return result;
+    }
+}
