89c0fd42f576dbd50f107766a2c39e5c221c5a35
proninp <proninpaul@yandex.ru>
2025-06-11 23:40:17 +0300

    Easy - 643. Maximum Average Subarray I

---
+++ C Sharp/LeetCode/LeetCode.Easy/0643. Maximum Average Subarray I/src/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Easy._0643._Maximum_Average_Subarray_I.src;
+
+public class Solution
+{
+    public double FindMaxAverage(int[] nums, int k)
+    {
+        var maxAvg = Double.MinValue;
+        var sum = 0.0;
+
+        var left = 0;
+
+        for (var right = 0; right < nums.Length; right++)
+        {
+            sum += nums[right];
+            if (right - left + 1 == k)
+            {
+                var avg = sum / (right - left + 1);
+                if (avg > maxAvg)
+                    maxAvg = avg;
+                sum -= nums[left];
+                left++;
+            }
+        }
+        return maxAvg;
+    }
+}
