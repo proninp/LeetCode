ad3c4598a8c708079042d16ca005588406e67468
proninp <proninpaul@yandex.ru>
2025-06-12 12:01:11 +0300

    Medium - 1456. Maximum Number of Vowels in a Substring of Given Length

---
+++ C Sharp/LeetCode/LeetCode.Medium/1456. Maximum Number of Vowels in a Substring of Given Length/src/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Medium._1456._Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length.src;
+
+public class Solution
+{
+    public int MaxVowels(string s, int k)
+    {
+        var vowels = new int[128];
+        foreach (var c in "aeiou")
+            vowels[c] = 1;
+
+        var max = 0;
+        for (var i = 0; i < k; i++)
+            max += vowels[s[i]];
+        var current = max;
+        for (var i = k; i < s.Length; i++)
+        {
+            current += vowels[s[i]];
+            current -= vowels[s[i - k]];
+            max = Math.Max(current, max);
+        }
+        return max;
+    }
+}
