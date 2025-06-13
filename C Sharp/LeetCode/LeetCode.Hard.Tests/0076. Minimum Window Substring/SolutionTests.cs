8c2b85387a6b0a2241fc562fd16b8f87f9505e04
proninp <proninpaul@yandex.ru>
2025-06-13 12:17:31 +0300

    Hard - 76. Minimum Window Substring

---
+++ C Sharp/LeetCode/LeetCode.Hard.Tests/0076. Minimum Window Substring/SolutionTests.cs
@@ -0,0 +1,17 @@
+﻿using LeetCode.Hard._0076._Minimum_Window_Substring.src;
+
+namespace LeetCode.Hard.Tests._0076._Minimum_Window_Substring;
+
+public class SolutionTests
+{
+    [Theory]
+    [InlineData("ADOBECODEBANC", "ABC", "BANC")]
+    [InlineData("ab", "b", "b")]
+    [InlineData("bba", "ab", "ba")]
+    public void BasicTests(string s, string t, string expected)
+    {
+        var solution = new Solution();
+        var actual = solution.MinWindow(s, t);
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ C Sharp/LeetCode/LeetCode.Hard/0076. Minimum Window Substring/src/Solution.cs
@@ -0,0 +1,60 @@
+﻿namespace LeetCode.Hard._0076._Minimum_Window_Substring.src;
+
+public class Solution
+{
+    public string MinWindow(string s, string t)
+    {
+        if (s.Length == t.Length && s.Equals(t))
+            return t;
+        
+        var sourceMap = new Dictionary<char, int>(t.Length);
+        foreach (var c in t)
+            if (!sourceMap.TryAdd(c, 1))
+                sourceMap[c]++;
+
+        var minLen = int.MaxValue;
+        var minLeft = -1;
+        var minRight = -1;
+        var need = sourceMap.Keys.Count;
+        var have = 0;
+        
+        var left = 0;
+        while (left < s.Length && !sourceMap.ContainsKey(s[left]))
+            left++;
+
+        if (left == s.Length) return string.Empty;
+
+        var windowMap = new Dictionary<char, int>(t.Length);
+        
+        for (var right = left; right < s.Length; right++)
+        {
+            if (sourceMap.ContainsKey(s[right]))
+            {
+                if (!windowMap.TryAdd(s[right], 1))
+                    windowMap[s[right]]++;
+                if (windowMap[s[right]] == sourceMap[s[right]])
+                    have++;
+            }
+
+            while (have >= need)
+            {
+                if (right - left + 1 < minLen)
+                {
+                    minLen = right - left + 1;
+                    minLeft = left;
+                    minRight = right;
+                }
+            
+                windowMap[s[left]]--;
+                if (windowMap[s[left]] < sourceMap[s[left]])
+                    have--;
+                do
+                {
+                    left++;
+                } while (left < right && !sourceMap.ContainsKey(s[left]));
+            }
+        }
+
+        return minLen == int.MaxValue ? string.Empty : s.Substring(minLeft, minRight - minLeft + 1);
+    }
+}
