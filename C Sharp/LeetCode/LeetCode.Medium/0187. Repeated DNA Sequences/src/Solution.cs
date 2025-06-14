997cde51bd29d931151606edfc08f48ba526388a
proninp <proninpaul@yandex.ru>
2025-06-14 21:48:22 +0300

    Medium - 187. Repeated DNA Sequences

---
+++ C Sharp/LeetCode/LeetCode.Medium/0187. Repeated DNA Sequences/src/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Medium._0187._Repeated_DNA_Sequences.src;
+
+public class Solution
+{
+    public IList<string> FindRepeatedDnaSequences(string s) {
+        var set = new HashSet<string>();
+        var repeated = new HashSet<string>(s.Length / 10);
+
+        for (var i = 0; i <= s.Length - 10; i++)
+        {
+            var key = s.Substring(i, 10);
+            if (!set.Add(key))
+                repeated.Add(key);
+        }
+        return new List<string>(repeated);
+    }
+}
