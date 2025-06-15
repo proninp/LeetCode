6abf11753f0693fef3b432ca9533c65522f364fa
proninp <proninpaul@yandex.ru>
2025-06-15 10:35:19 +0300

    Hard - 239. Sliding Window Maximum

---
+++ C Sharp/LeetCode/LeetCode.Hard/0239. Sliding Window Maximum/src/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Hard._0239._Sliding_Window_Maximum.src;
+
+public class Solution
+{
+    public int[] MaxSlidingWindow(int[] nums, int k) {
+        var result = new List<int>(nums.Length > 2 ? nums.Length - 2 : 1);
+        var deque = new LinkedList<int>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (deque.Count > 0 && deque.First!.Value <= i - k)
+                deque.RemoveFirst();
+
+            while (deque.Count > 0 && nums[deque.Last!.Value] <= nums[i])
+                deque.RemoveLast();
+
+            deque.AddLast(i);
+            
+            if (i >= k - 1)
+                result.Add(nums[deque.First!.Value]);
+        }
+        return result.ToArray();
+    }
+}
