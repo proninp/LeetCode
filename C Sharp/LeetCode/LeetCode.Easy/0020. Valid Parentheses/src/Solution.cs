e5842fff48b64cd3857fcbee5bd8c93608093f48
proninp <proninpaul@yandex.ru>
2025-06-16 10:47:46 +0300

    Easy - 20. Valid Parentheses

---
+++ C Sharp/LeetCode/LeetCode.Easy/0020. Valid Parentheses/src/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Easy._0020._Valid_Parentheses.src;
+
+public class Solution
+{
+    public bool IsValid(string s)
+    {
+        var stack = new Stack<char>();
+        foreach (var c in s)
+        {
+            if(c is '(' or '[' or '{')
+            {
+                stack.Push(c);
+            }
+            else
+            {
+                if (stack.Count == 0)
+                    return false;
+                var top = stack.Pop();
+                if ((c == ')' && top != '(') ||
+                    (c == ']' && top != '[') ||
+                    (c == '}' && top != '{'))
+                {
+                    return false;
+                }
+            }
+        }
+        return stack.Count == 0;
+    }
+}
