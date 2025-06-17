43004b00b2ae3ea30af08c9bb6da8714b12f5cf2
proninp <proninpaul@yandex.ru>
2025-06-17 19:17:16 +0300

    Medium - 155. Min Stack

---
+++ C Sharp/LeetCode/LeetCode.Medium/0155. Min Stack/src/MinStack.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Medium._0155._Min_Stack.src;
+
+public class MinStack
+{
+    private Stack<(int Val, int Min)> _stack;
+    
+    public MinStack() {
+        _stack = new Stack<(int, int)>();
+    }
+    
+    public void Push(int val)
+    {
+        var min = _stack.Count > 0 ? _stack.Peek().Min : int.MaxValue;
+        _stack.Push((val, Math.Min(min, val)));
+    }
+    
+    public void Pop() {
+        _stack.Pop();
+    }
+    
+    public int Top() {
+        return _stack.Peek().Val;
+    }
+    
+    public int GetMin() {
+        return _stack.Peek().Min;
+    }
+}
