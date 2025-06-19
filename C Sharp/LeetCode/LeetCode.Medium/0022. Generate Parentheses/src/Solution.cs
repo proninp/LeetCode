629bd010b73de398555d49fe5d5fa157feb173f8
proninp <proninpaul@yandex.ru>
2025-06-19 10:26:04 +0300

    Medium - 22. Generate Parentheses

---
+++ C Sharp/LeetCode/LeetCode.Medium/0022. Generate Parentheses/src/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Medium._0022._Generate_Parentheses.src;
+
+public class Solution
+{
+    public IList<string> GenerateParenthesis(int n)
+    {
+        var result = new List<string>();
+        GenerateRecursive(n - 1, n, "(", result);
+        return result;
+    }
+
+    private void GenerateRecursive(int openedRemain, int closedRemain, string s, IList<string> result)
+    {
+        if (openedRemain == 0 && closedRemain == 0)
+            result.Add(s);
+        else
+        {
+            if (openedRemain > 0)
+                GenerateRecursive(openedRemain - 1, closedRemain, s + "(", result);
+            if (openedRemain < closedRemain)
+                GenerateRecursive(openedRemain, closedRemain - 1, s + ")", result);
+        }
+    }
+}
