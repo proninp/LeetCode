ba506ac61789e320cfad657b57ad4fe10ed8cdbb
proninp <proninpaul@yandex.ru>
2025-06-20 11:36:23 +0300

    Medium - 739. Daily Temperatures

---
+++ C Sharp/LeetCode/LeetCode.Medium/0739. Daily Temperatures/src/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+
+namespace LeetCode.Medium._0739._Daily_Temperatures.src;
+
+public class Solution
+{
+    public int[] DailyTemperatures(int[] temperatures) {
+        var results = new int[temperatures.Length];
+        var stack = new Stack<int>();
+        for (var i = 0; i < temperatures.Length; i++)
+        {
+            while (stack.Count > 0 && temperatures[stack.Peek()] < temperatures[i])
+            {
+                var idx = stack.Pop();
+                results[idx] = i - idx;
+            }
+            stack.Push(i);
+        }
+        return results;
+    }
+}
