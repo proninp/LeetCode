864a831e1b5ed285d5d050d39b703465bde8452c
proninp <proninpaul@yandex.ru>
2025-06-28 12:25:34 +0300

    Hard - 84. Largest Rectangle in Histogram

---
+++ C Sharp/LeetCode/LeetCode.Hard/0084. Largest Rectangle In Histogram/src/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Hard._0084._Largest_Rectangle_In_Histogram.src;
+
+public class Solution
+{
+    public int LargestRectangleArea(int[] heights)
+    {
+        var max = 0;
+        var len = heights.Length;
+        var stack = new Stack<int>();
+        
+        for (var i = 0; i <= len; i++)
+        {
+            while (stack.Count > 0 && (i == len || heights[i] < heights[stack.Peek()]))
+            {
+                var height = heights[stack.Pop()];
+                var width = stack.Count == 0 ? i : i - stack.Peek() - 1;
+                max = Math.Max(max, height * width);
+            }
+            stack.Push(i);
+        }
+        return max;
+    }
+}
