a166f04d09fc5526ef3ef6d59fed20722d83a767
proninp <proninpaul@yandex.ru>
2025-07-02 08:36:28 +0300

    Easy 704. Binary Search

---
+++ C Sharp/LeetCode/LeetCode.Easy/0704. Binary Search/src/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Easy._0704._Binary_Search.src;
+
+public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        int left = 0, right = nums.Length - 1;
+        while (left <= right)
+        {
+            var mid = left + (right - left) / 2;
+            if (nums[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else if (nums[mid] > target)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
