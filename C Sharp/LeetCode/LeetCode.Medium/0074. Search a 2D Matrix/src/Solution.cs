f0a0699596275b57915c81c885afbf3b31705fb1
proninp <proninpaul@yandex.ru>
2025-07-03 08:49:20 +0300

    Medium - 74. Search a 2D Matrix

---
+++ C Sharp/LeetCode/LeetCode.Medium/0074. Search a 2D Matrix/src/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Medium._0074._Search_a_2D_Matrix.src;
+
+public class Solution
+{
+    public bool SearchMatrix(int[][] matrix, int target)
+    {
+        foreach (var matrixRow in matrix)
+        {
+            if (target > matrixRow[matrixRow.Length - 1]) continue;
+            
+            int left = 0, right = matrixRow.Length - 1;
+            while (left <= right)
+            {
+                var mid = left + (right - left) / 2;
+                if (matrixRow[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else if (matrixRow[mid] > target)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
