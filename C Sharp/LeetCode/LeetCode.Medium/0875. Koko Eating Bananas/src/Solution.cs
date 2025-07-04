c8b857889ad0c231f3724532e9f5e24760430597
proninp <proninpaul@yandex.ru>
2025-07-04 09:24:16 +0300

    Medium - 875. Koko Eating Bananas

---
+++ C Sharp/LeetCode/LeetCode.Medium/0875. Koko Eating Bananas/src/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Medium._0875._Koko_Eating_Bananas.src;
+
+public class Solution
+{
+    public int MinEatingSpeed(int[] piles, int h)
+    {
+        var k = piles.Max();
+        
+        var left = 1;
+        var right = k;
+        while (left <= right)
+        {
+            var mid = left + (right - left) / 2;
+            long totalTime = 0;
+            foreach (var p in piles)
+                totalTime += (p + mid - 1) / mid;
+
+            if (totalTime <= h)
+            {
+                k = mid;
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        return k;
+    }
+}
