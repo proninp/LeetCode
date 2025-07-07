ce1ebb4a36d434e182d41624c3bb47aa58b509ac
proninp <proninpaul@yandex.ru>
2025-07-07 08:57:24 +0300

    Medium - 153. Find Minimum in Rotated Sorted Array

---
+++ C Sharp/LeetCode/LeetCode.Medium/0153. Find Minimum in Rotated Sorted Array/src/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Medium._0153._Find_Minimum_in_Rotated_Sorted_Array.src;
+
+public class Solution
+{
+    public int FindMin(int[] nums) {
+        var min = nums[0];
+        int l = 0, r = nums.Length - 1;
+        while (l <= r)
+        {
+            if (nums[l] < nums[r])
+            {
+                min = Math.Min(min, nums[l]);
+                break;
+            }
+
+            var mid = (r + l) / 2;
+            min = Math.Min(min, nums[mid]);
+            if (nums[l] <= nums[mid])
+            {
+                l = mid + 1;
+            }
+            else
+            {
+                r = mid - 1;
+            }
+        }
+        return min;
+    }
+}
