f0391e1de5a1e680757fd0047ef13b11c488c38f
proninp <proninpaul@yandex.ru>
2025-07-08 11:43:39 +0300

    Medium - 33. Search in Rotated Sorted Array

---
+++ C Sharp/LeetCode/LeetCode.Medium.Tests/0033. Search in Rotated Sorted Array/SolutionTest.cs
@@ -0,0 +1,17 @@
+﻿using LeetCode.Medium._0033._Search_in_Rotated_Sorted_Array.src;
+
+namespace LeetCode.Medium.Tests._0033._Search_in_Rotated_Sorted_Array;
+
+public class SolutionTest
+{
+    [Theory]
+    [InlineData(new int[] { 3, 1 }, 1, 1)]
+    [InlineData(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 0, 4)]
+    public void TestExample1(int[] nums, int target, int expected)
+    {
+        var solution = new Solution();
+        var result = solution.Search(nums, target);
+
+        Assert.Equivalent(expected, result);
+    }
+}
---
+++ C Sharp/LeetCode/LeetCode.Medium/0033. Search in Rotated Sorted Array/src/Solution.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode.Medium._0033._Search_in_Rotated_Sorted_Array.src;
+
+public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        int result = -1, l = 0, r = nums.Length - 1;
+        while (l <= r)
+        {
+            var mid = (l + r) / 2;
+            if (target == nums[mid]) return mid;
+            if (nums[l] <= nums[mid])
+            {
+                if (target >= nums[l] && target < nums[mid])
+                {
+                    r = mid - 1;
+                }
+                else
+                {
+                    l = mid + 1;
+                }    
+            }
+            else
+            {
+                if (target > nums[mid] && target <= nums[r])
+                {
+                    l = mid + 1;
+                }
+                else
+                {
+                    r = mid - 1;
+                }
+            }
+        }
+        return result;
+    }
+}
