ec61580e566529e0e484241929eea79f9e9a3bef
proninp <proninpaul@yandex.ru>
2025-07-10 13:34:56 +0300

    Easy - 344. Reverse String

---
+++ C Sharp/LeetCode/LeetCode.Easy/0344. Reverse String/src/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.Easy._0344._Reverse_String.src;
+
+public class Solution
+{
+    public void ReverseString(char[] s)
+    {
+        int left = 0, right = s.Length - 1;
+        while (left < right)
+        {
+            (s[left], s[right]) = (s[right], s[left]);
+            left++;
+            right--;
+        }
+    }
+}
