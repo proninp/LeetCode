390546b7768f6d466e78eb6db7ed6e47129f8d3d
proninp <proninpaul@yandex.ru>
2025-07-11 17:10:45 +0300

    Easy - 94. Binary Tree Inorder Traversal

---
+++ C Sharp/LeetCode/LeetCode.Easy/0094. Binary Tree Inorder Traversal/src/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.Easy._0094._Binary_Tree_Inorder_Traversal.src;
+
+public class Solution
+{
+    public IList<int> InorderTraversal(TreeNode root)
+    {
+        var result = new List<int>();
+        Traverse(result, root);
+        return result;
+    }
+
+    private void Traverse(IList<int> list, TreeNode? node)
+    {
+        if (node == null) return;
+        Traverse(list, node.left);
+        list.Add(node.val);
+        Traverse(list, node.right);
+    }
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
