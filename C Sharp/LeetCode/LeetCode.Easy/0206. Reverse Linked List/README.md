a5df7330ac960bc2d5aae60a08d6bf8619c66fce
proninp <proninpaul@yandex.ru>
2025-07-11 09:01:14 +0300

    Easy - 206. Reverse Linked List

---
+++ C Sharp/LeetCode/LeetCode.Easy/0206. Reverse Linked List/README.md
@@ -0,0 +1,24 @@
+# [206. Reverse Linked List](https://leetcode.com/problems/reverse-linked-list/description/)
+
+Given the `head` of a singly linked list, reverse the list, and return _the reversed list_.
+
+**Example 1:**
+
+![example1.png](res/ex1.png)
+
+**Input:** `head = [1,2,3,4,5]`
+**Output:** `[5,4,3,2,1]`
+
+
+**Example 2:**
+
+![example2.png](res/ex2.png)
+
+**Input:** `head = [1,2]`
+**Output:** `[2,1]`
+
+
+**Example 3:**
+
+**Input:** `head = []`
+**Output:** `[]`
---
+++ C Sharp/LeetCode/LeetCode.Easy/0206. Reverse Linked List/src/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Easy._0206._Reverse_Linked_List.src;
+
+public class Solution
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        ListNode prev = null;
+        var current = head;
+        while (current != null)
+        {
+            var tmp =  current.next;
+            current.next = prev;
+            prev = current;
+            current = tmp;
+        }
+        return prev;
+    }
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
